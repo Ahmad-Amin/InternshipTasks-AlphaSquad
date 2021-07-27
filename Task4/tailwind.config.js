13cfc2b51cb88db1aab626a85fbd113150434130
Ahmad-Amin <baigahmad323@gmail.com>
2021-07-27 18:33:31 +0500

    Completed the 2 product Details page section

--- Task4/tailwind.config.js
+++ Task4/tailwind.config.js
@@ -3,9 +3,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundImage: theme => ({
+        'shoeImage': "url('/assets/Rectangle 5shoes.png')",
+      }),
       fontFamily:{
         'WorkSans': ['Work Sans','sans-serif']
-      }
+      },
+      
     },
   },
   variants: {
